3083554a9762a6bc798a65dca6474a0dc4bd7979
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2022-10-23 23:31:28 +0100

    Created form and linked to home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,23 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.forms import ModelForm
+from django.forms import Textarea
+from django.forms import NumberInput
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your forms here.
+class ThingsForm(ModelForm):
+
+    class Meta:
+        model = Thing
+        fields = ['name', 'description','quantity']
+
+        widgets = {
+            'description': Textarea(),
+            'quantity': NumberInput(),
+        }
+
+
+
