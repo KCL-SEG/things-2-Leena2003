642064a48376983619578292e818c3d3273fc417
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2022-10-24 00:00:59 +0100

    corrected spelling mistake

--- things/forms.py
+++ things/forms.py
@@ -4,11 +4,11 @@ from .models import Thing
 from django.forms import ModelForm
 from django.forms import Textarea
 from django.forms import NumberInput
-from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 
 # Create your forms here.
-class ThingsForm(ModelForm):
+class ThingForm(ModelForm):
 
     class Meta:
         model = Thing
